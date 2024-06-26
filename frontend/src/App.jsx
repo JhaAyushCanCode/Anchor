9dc35c8ead903409e48da6962adf1ba344b87982
JhaAyushCanCode <flyingayush2@gmail.com>
2024-06-26 23:35:11 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,60 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner";
+
+function App() {
+    const { data: authUser, isLoading, error } = useQuery({
+        queryKey: ["authUser"],
+        queryFn: async () => {
+            const res = await fetch("/api/auth/me");
+            if (!res.ok) {
+                const errorData = await res.json().catch(() => {
+                    throw new Error("Invalid JSON response");
+                });
+                throw new Error(errorData.error || "Something went wrong");
+            }
+            return res.json();
+        },
+        retry: false,
+    });
+
+    if (isLoading) {
+        return (
+            <div className='h-screen flex justify-center items-center'>
+                <LoadingSpinner size='lg' />
+            </div>
+        );
+    }
+
+    if (error) {
+        console.error("Error fetching auth user:", error.message);
+    }
+
+    return (
+        <div className='flex max-w-6xl mx-auto'>
+            {authUser && <Sidebar />}
+            <Routes>
+                <Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+                <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/' />} />
+                <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to='/' />} />
+                <Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to='/login' />} />
+                <Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
+            </Routes>
+            {authUser && <RightPanel />}
+            <Toaster />
+        </div>
+    );
+}
+
+export default App;
